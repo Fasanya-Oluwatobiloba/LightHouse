2ec05dca7de7aed5a9917b076396212f95102be7
Your Name <you@example.com>
2025-05-23 12:07:59 +0100

    edited app.jsx because of the capital letter. i changed it to small letter (pages)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Home from "./Pages/Home";
-import Sermons from "./Pages/Sermon";
-import SermonDetail from "./Pages/SermonDetail";
+import Home from "./pages/Home";
+import Sermons from "./pages/Sermon";
+import SermonDetail from "./pages/SermonDetail";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Events from "./pages/Events";
 import Gallery from "./pages/Gallery";
 import Login from "./pages/admin/Login";
-import Dashboard from "./Pages/Admin/Dashboard";
-import UploadSermon from "./Pages/Admin/UploadSermon";
+import Dashboard from "./pages/Admin/Dashboard";
+import UploadSermon from "./pages/Admin/UploadSermon";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function App() {
